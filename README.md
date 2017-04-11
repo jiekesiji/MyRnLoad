fe46bd5ab062b3904058132295c337495c291b9e
程杰 <jie.cheng@utsoft.cn>
2017-04-11 10:55:51 +0800

    更换编译版本  解决无法安装到真机上且白屏的问题

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MyRnLoad
 RN学习记录
+
+day01
+1.无法安装到真机上的解决方案 http://blog.csdn.net/mapboo/article/details/53419838
+2.运行到真机上出现白屏现象  需要打开应用的悬浮窗权限
--- SetUp.js
+++ SetUp.js
@@ -1,7 +1,5 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * 例子程序
  */
 
 import React, { Component } from 'react';
